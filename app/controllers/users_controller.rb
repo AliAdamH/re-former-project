75c35986026f60d2fe86e95aea1b3e48aabd6397
Ali Hassaine <ali.hs@outlook.fr>
2022-02-12 15:32:15 +0000

    Add update method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
 
   private
 
