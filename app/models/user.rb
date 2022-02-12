32feaf641ec79be486f21d3231f7d8f219e3508c
Ali Hassaine <ali.hs@outlook.fr>
2022-02-12 12:32:34 +0000

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, :email, presence: true, uniqueness: true
+  validates :username, length: { in: 3..20}
+  validates :password, presence: true, length: { in: 8..32 }
 end
